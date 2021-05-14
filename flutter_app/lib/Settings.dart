68189e7b65fab45d17b949edfac38f09528f1045
Rayvaldi <70924622+Rayvaldi@users.noreply.github.com>
2021-05-14 12:27:01 +0700

    update setting

--- flutter_app/lib/Settings.dart
+++ flutter_app/lib/Settings.dart
@@ -2,14 +2,97 @@ import 'package:flutter/material.dart';
 
 class Settings extends StatefulWidget {
   @override
-  _SettingsState createState() => _SettingsState();
+  _SettingState createState() => _SettingState();
 }
 
-class _SettingsState extends State<Settings> {
+class _SettingState extends State<Settings> {
+  late List items;
+
+  @override
+  void initState() {
+    super.initState();
+    items = [
+      {
+        'title': 'Favorites',
+      },
+      {
+        'title': 'Downloads',
+      },
+      {
+        'title': 'Dark Mode',
+      },
+      {
+        'title': 'About',
+      },
+      {
+        'title': 'Licenses',
+      },
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    // Remove Dark Switch if Device has Dark mode enabled
+    if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+      items.removeWhere((item) => item['title'] == 'Dark Mode');
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Settings',
+        ),
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (items[index]['title'] == 'Dark Mode') {
+          }
+
+          return ListTile(
+            onTap: items[index]['function'],
+            leading: Icon(
+              items[index]['icon'],
+            ),
+            title: Text(
+              items[index]['title'],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
+        },
+      ),
+    );
+  }
+
+
+  showAbout() {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: Text(
+            'About',
+          ),
+          content: Text(
+            'Simple Music app by Mobile Programming Kelompok ',
+          ),
+          actions: <Widget>[
+            FlatButton(
+              textColor: Theme.of(context).accentColor,
+              onPressed: () => Navigator.pop(context),
+              child: Text(
+                'Close',
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
